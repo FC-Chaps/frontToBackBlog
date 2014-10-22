4e31ae8c42a2921c3b324f93c5867cf85393d58c
mwgdavies <mwgdavies@users.noreply.github.com>
2014-10-22 16:31:01 +0100

    Mongolab uri

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 
 var mongodb = require('mongodb'),
-    uri = 'mongodb://heroku_app30917416:jf75lna2ssor5uptu23b3stgam@ds047050.mongolab.com:47050/heroku_app3091741';
+    uri = 'MONGOLAB_URI';
 
 mongodb.MongoClient.connect(uri, function(err, db) {
   if(err) throw err;
